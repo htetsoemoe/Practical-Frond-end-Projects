666d03003045245e9ecce0c2beacc48356f7429c
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-01-23 17:15:42 +0630

    finished show hide table and print btn

--- invoice/js/app.js
+++ invoice/js/app.js
@@ -23,68 +23,118 @@ const services = [
 ];
 
 // selector
+const app = document.querySelector("#app");
 const invoiceForm = document.querySelector("#invoiceForm")
 const selectService = document.querySelector("#selectService");// selector
 const qunatity = document.querySelector("#quantity");
+const listTable = document.querySelector("#list-table");
 const lists = document.querySelector("#lists");// table body
 const subTotal = document.querySelector("#subTotal");
 const tax = document.querySelector("#tax");
 const total = document.querySelector("#total");
 
 
+
+
 // logics
 // add services to select option
-services.forEach(service => {
-    selectService.append(new Option(service.title, service.id));
+services.forEach(({ title, id }) => {
+  selectService.append(new Option(title, id))
 });
 
 // create row with data in table
 const createTr = (service, qunatity) => {
-    const tr = document.createElement("tr");
-    const total = service.price * qunatity;
-    tr.innerHTML = `
-        <td>${service.title}</td>
-        <td class="text-end">${qunatity}</td>
+  const tr = document.createElement("tr");
+  tr.classList.add("list");
+  tr.setAttribute("service-id", service.id);
+  const total = service.price * qunatity;
+  tr.innerHTML = `
+        <td class="d-flex justify-content-between">
+          ${service.title}
+          <i class="bi bi-trash3 text-danger del-btn"></i>
+        </td>
+        <td class="text-end list-quantity">${qunatity}</td>
         <td class="text-end">${service.price}</td>
         <td class="text-end list-total">${total}</td>
     `;
 
-    return tr;
+  return tr;
 };
 
 // calculate tax
 const calculateTax = (amount, percentage = 5) => {
-    return amount * (percentage/100);
+  return amount * (percentage / 100);
 }
 
 // calculate total
 const findTotal = () => {
-    // const listTotal = document.querySelectorAll(".list-total");
-    let valueOfSubTotal = [...document.querySelectorAll(".list-total")].reduce((pv,cv)=> pv + parseFloat(cv.innerText), 0);
-   
-    subTotal.innerText = valueOfSubTotal;
-    tax.innerText = calculateTax(valueOfSubTotal);
-    total.innerText = valueOfSubTotal + calculateTax(valueOfSubTotal);
+  // const listTotal = document.querySelectorAll(".list-total");
+  let valueOfSubTotal = [...document.querySelectorAll(".list-total")].reduce((pv, cv) => pv + parseFloat(cv.innerText), 0);
+
+  subTotal.innerText = valueOfSubTotal;
+  tax.innerText = calculateTax(valueOfSubTotal);
+  total.innerText = valueOfSubTotal + calculateTax(valueOfSubTotal);
 
 };
 
+// show table
+const showtable = () => {
+  if (lists.children.length) {
+    listTable.classList.remove("d-none");
+  } else {
+    listTable.classList.add("d-none");
+  }
+}
+
 // get all data from 'form inputs'
 invoiceForm.addEventListener("submit", event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    //console.log(selectService.value, qunatity.valueAsNumber, selectedService);
-    
-    // find selected service object 
-    const selectedService = services.find(service => service.id == selectService.value);
-    console.log(selectedService);
+  //console.log(selectService.value, qunatity.valueAsNumber, selectedService);
 
+  // find selected service object 
+  const selectedService = services.find(({ id }) => id == selectService.value);
+  console.log(selectedService);
+
+  // check one of the service type (row) is already exist in table
+  const isExistedService = [...lists.children].find(
+    el => el.getAttribute("service-id") == selectedService.id
+  );
+
+  if (isExistedService) {
+    console.log("already exist");
+    console.log(isExistedService);
+
+    // update quantity and total of existed service
+    const existedQuantity = isExistedService.querySelector(".list-quantity");
+
+    existedQuantity.innerText = parseFloat(existedQuantity.innerText) + qunatity.valueAsNumber;
+    isExistedService.querySelector(".list-total").innerText = existedQuantity.innerText * selectedService.price;
+
+
+  } else {
     // generate row 
     lists.append(createTr(selectedService, qunatity.valueAsNumber));
+  }
 
-    // calculate value and show result
+  // calculate value and show result
+  findTotal();
+
+  // clear data from inputs
+  invoiceForm.reset();
+
+  // show table
+  showtable();
+});
+
+// delete function
+app.addEventListener("click", event => {
+  const currentElement = event.target;
+  if (currentElement.classList.contains("del-btn")) {
+    // delet the row when delet button have been clicked
+    currentElement.closest("tr").remove();
     findTotal();
-
-    // clear data from inputs
-    invoiceForm.reset();
-})
+    showtable();
+  }
+});
 
