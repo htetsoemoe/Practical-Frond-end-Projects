75642c52ad994d483f5bf470fdbd2921d76e2adb
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-01-28 14:04:25 +0630

    updated text-controller project with web speech and recognition api

--- text-controller/text-controller.js
+++ text-controller/text-controller.js
@@ -8,6 +8,11 @@ const count = document.querySelector("#count");
 const color = document.querySelector("#color");
 const fontSize = document.querySelector("#fontSize");
 const fontFamily = document.querySelector("#fontFamily");
+const speechToText = document.querySelector("#speechToText");
+const textToSpeech = document.querySelector("#textToSpeech");
+
+// variable for text to speech
+const synth = window.speechSynthesis;
 
 // set font to select option
 fonts.forEach(font => {
@@ -38,3 +43,65 @@ fontSize.addEventListener("change", event => {
 fontFamily.addEventListener("change", event => {
     output.style.fontFamily = event.target.value;
 });
+
+/**
+ *  Functions for TextToSpeech using Web Speech API
+ */
+
+const speak = text => {
+    const utterThis = new SpeechSynthesisUtterance();
+    utterThis.rate = 0.8;
+    utterThis.text = text;
+    utterThis.voice = synth.getVoices()[1];
+
+    utterThis.addEventListener("start", () => {
+        textToSpeech.classList.add("active");
+    });
+
+    utterThis.addEventListener("end", () => {
+        textToSpeech.classList.remove("active");
+    });
+
+    synth.speak(utterThis);
+};
+
+textToSpeech.addEventListener("click", () => {
+    speak(text.value);
+});
+
+/**
+ *  Functions for SpeechToText using Speech Recognition API
+ * 
+ */
+
+const listen = () => {
+    var recognition = new webkitSpeechRecognition();
+
+    // Set the language and start recognizing
+    recognition.lang = "en-US";
+    recognition.start();
+
+    // When a speech result is returned
+    recognition.onresult = function (event) {
+        // Get the transcript of the spoken words
+        // console.log(event.results);
+        var transcript = event.results[0][0].transcript;
+        text.value += transcript;
+    };
+
+    recognition.addEventListener("start", () => {
+        speechToText.classList.add("active");
+        speechToText.innerHTML = `
+            <div class="spinner-border text-white spinner-border-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        `;
+    });
+
+    recognition.addEventListener("end", () => {
+        speechToText.classList.remove("active");
+        speechToText.innerHTML = `<i class="bi bi-mic"></i>`;
+    });
+};
+
+speechToText.addEventListener("click", listen);
